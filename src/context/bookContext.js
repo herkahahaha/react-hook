4f333f939305ef3b26257e52149f93ae413f1ace
Herkahahaha <herkahahaha@gmail.com>
2019-10-08 22:14:01 +0700

    add saving data with useEffect

--- src/context/bookContext.js
+++ src/context/bookContext.js
@@ -1,11 +1,18 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { bookReducer } from "../reducer/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, dispatch] = useReducer(bookReducer, []);
-
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    // add saving data in our browser
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
+  useEffect(() => {
+    // render data to our localstorage
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
   return (
     <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
